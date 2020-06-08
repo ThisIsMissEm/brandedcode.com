7b8ad0cb0da495fcee5f554466c055827a34a7a2
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2020-06-08 18:18:08 +0200

    Update website with newer information

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,22 +27,24 @@ const IndexPage = () => (
         return (
           <header>
             <h1 style={{ fontSize: matches ? '60px' : '48px', fontWeight: 800, color: '#6833DD' }}>BrandedCode</h1>
-            <h2 style={{ fontSize: matches ? '24px' : '20px', fontWeight: 800 }}>The former brand of Emelia Smith</h2>
+            <h2 style={{ fontSize: matches ? '24px' : '20px', fontWeight: 800 }}>The online presence of Emelia Smith</h2>
           </header>
         )
       }}
     </Responsive>
     <section style={{ color: '#666' }}>
-      <p>After almost a decade as a software engineer, I have decided to stop working in the tech industry.</p>
-      <p>On April 4th 2017, I published <a href="https://medium.com/@thisismissem/tech-this-is-good-bye-19b88b2a760f" title="Read the article “Tech, This Is Good Bye.”">an article</a> detailing my story of working in the tech industry, and stating that I'd be leaving the industry. Over twenty thousand people viewed the article, and ten thousand people read it, according to Medium. People tried to encourage me to stay in the tech industry, but I knew that it was time for a new chapter in my life.</p>
-      <p>After choosing to no longer work in tech, I've decided to work in the Adult Industry. Occasionally I still do the odd bit of consulting within the tech industry, as it does still pay money, but overall, I've left the tech industry.</p>
-      <p>I've started a company called <a href="https://unobvious.technology" title="Learn more about Unobvious Technology">Unobvious Technology</a>, dedicated to bringing <span title="(like those I used to build in the tech industry)">modern, safe, and secure services</span> to the adult industry and the sex workers within it.</p>
+      <p>Hi, I'm Emelia, a highly experienced software engineer and founder based in Berlin, Germany</p>
+      <p>I'm perhaps most known for my voice on <a href="https://twitter.com/thisismissem" title="Follow Emelia on Twitter">Twitter</a>, where I talk about Tech, Politics, Distributed Web, rights for sex workers and equality. You can find my writing on <a href="https://medium.com/@thisismissem" title="Read articles by Emelia on Medium">Medium</a>, when I publish it. You may also know me as the <a href="https://www.instagram.com/p/B6B4vVRIA22/" title="Photo of Emelia dressed as a princess at React Day Berlin 2020">tech princess</a>, or the <a href="https://medium.com/@thisismissem/tech-this-is-good-bye-19b88b2a760f" title="Read the article about Emelia leaving tech">trans woman who left tech</a> (I'm back).</p>
+      <p>For the past three years, I have been bootstrapping a company called <a href="https://unobvious.technology" title="Learn more about Unobvious Technology">Unobvious Technology</a>, dedicated to bringing <span title="(like those I've built in the tech industry)">modern, safe, and secure services</span> to sex workers and the adult industry.</p>
 
       <Button href="https://unobvious.technology" isPrimary title="Learn more about Unobvious Technology">Unobvious Technology</Button>
       <Button href="mailto:emelia@brandedcode.com?subject=Hi!">Contact Emelia</Button>
     </section>
     <footer style={{ marginTop: '60px', borderTop: '1px solid #ccc', padding: '6px 0 0', color: '#666', fontSize: 'smaller'}}>
-      <p><a href="https://github.com/thisismissem" title="Emelia on Github" style={{color: '#aaa'}}>Github</a> / <a href="https://twitter.com/thisismissem" title="Follow on Twitter" style={{color: '#aaa'}}>Twitter</a> / <a href="https://www.linkedin.com/in/thisismissem" title="See my previous work on LinkedIn" style={{color: '#aaa'}}>LinkedIn</a></p>
+      <p>
+        <a href="https://github.com/thisismissem" title="Emelia on Github" style={{color: '#aaa'}}>Github</a> / <a href="https://twitter.com/thisismissem" title="Follow on Twitter" style={{color: '#aaa'}}>Twitter</a> / <a href="https://www.linkedin.com/in/thisismissem" title="See my previous work on LinkedIn" style={{color: '#aaa'}}>LinkedIn</a>
+        <span style={{ float: 'right' }}>Last Updated 8th June 2020</span>
+      </p>
     </footer>
   </div>
 )
