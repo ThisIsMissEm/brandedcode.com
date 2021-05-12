0db69199fd4de728dc5e79a90b77e033f70eddfa
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2021-05-12 22:23:16 +0200

    feat: add email link, but guard with confirmation

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,8 @@ const Button = ({ isPrimary, ...props}) => (
   }} {...props} />
 )
 
+const Today = new Date()
+
 const IndexPage = () => (
   <div style={{color: '#222' }}>
     <Responsive minDeviceWidth={600}>
@@ -34,15 +36,25 @@ const IndexPage = () => (
     </Responsive>
     <section style={{ color: '#666' }}>
       <p>Hi, I'm Emelia, a highly experienced software engineer and founder based in Berlin, Germany</p>
-      <p>I'm perhaps most known for my voice on <a href="https://twitter.com/thisismissem" title="Follow Emelia on Twitter">Twitter</a>, where I talk about Tech, Politics, Distributed Web, rights for sex workers and equality. You can find my writing on <a href="https://medium.com/@thisismissem" title="Read articles by Emelia on Medium">Medium</a>, when I publish it. You may also know me as the <a href="https://www.instagram.com/p/B6B4vVRIA22/" title="Photo of Emelia dressed as a princess at React Day Berlin 2020">tech princess</a>, or the <a href="https://medium.com/@thisismissem/tech-this-is-good-bye-19b88b2a760f" title="Read the article about Emelia leaving tech">trans woman who left tech</a> (I'm back)</p>
-      <p>For the past three years, I have been bootstrapping a company called <a href="https://unobvious.technology" title="Learn more about Unobvious Technology">Unobvious Technology</a>, dedicated to bringing <span title="(like those I've built in the tech industry)">modern, safe, and secure services</span> to sex workers and the adult industry</p>
+      <p>I'm perhaps most known for my voice on <a href="https://twitter.com/thisismissem" title="Follow Emelia on Twitter">Twitter</a>, where I talk about Tech, Politics, Distributed Web, rights for sex workers and equality. You can find my writing on <a href="https://medium.com/@thisismissem" title="Read articles by Emelia on Medium">Medium</a>, when I publish it.</p>
+      <p>You may also know me as the <a href="https://www.instagram.com/p/B6B4vVRIA22/" title="Photo of Emelia dressed as a princess at React Day Berlin 2020">tech princess</a>, or the <a href="https://medium.com/@thisismissem/tech-this-is-good-bye-19b88b2a760f" title="Read the article about Emelia leaving tech">trans woman who left tech</a> (I'm back).</p>
+      <p>For the past {Today.getFullYear() - 2017} years, I have been bootstrapping my own company called <a href="https://unobvious.technology" title="Learn more about Unobvious Technology">Unobvious Technology UG</a>, dedicated to bringing <span title="(like those I've built in the tech industry)">modern, safe, and secure services</span> to sex workers and the adult industry.</p>
 
       <Button href="https://unobvious.technology" isPrimary title="Learn more about Unobvious Technology">Unobvious Technology</Button>
-      <Button href="https://www.linkedin.com/in/thisismissem/">Hire Emelia</Button>
+      <Button href="https://www.linkedin.com/in/thisismissem/">Hire Emelia*</Button>
+
+      <p><small>* If she is available</small></p>
     </section>
     <footer style={{ marginTop: '60px', borderTop: '1px solid #ccc', padding: '6px 0 0', color: '#666', fontSize: 'smaller'}}>
       <p>
-        <a href="https://github.com/thisismissem" title="Emelia on Github" style={{color: '#aaa'}}>Github</a> / <a href="https://twitter.com/thisismissem" title="Follow on Twitter" style={{color: '#aaa'}}>Twitter</a> / <a href="https://www.linkedin.com/in/thisismissem" title="See my previous work on LinkedIn" style={{color: '#aaa'}}>LinkedIn</a>
+        <a href="https://github.com/thisismissem" title="Emelia on Github" style={{color: '#aaa'}}>Github</a> / <a href="https://twitter.com/thisismissem" title="Follow on Twitter" style={{color: '#aaa'}}>Twitter</a> / <a href="https://www.linkedin.com/in/thisismissem" title="See my previous work on LinkedIn" style={{color: '#aaa'}}>LinkedIn</a> / <a href="#" title="Contact Emelia for non-recruitment matters" style={{color: '#aaa'}} onClick={(e) => {
+          e.preventDefault();
+          if(window.confirm('Is this about recruitment or hiring? (press ok)')) {
+            window.alert("Please use the 'Hire Emelia' button instead");
+          } else {
+            window.location.href = 'mailto:emelia@brandedcode.com?subject=Hi'
+          }
+        }}>Email</a>
         <span style={{ float: 'right' }}>Last Updated 8th June 2020</span>
       </p>
     </footer>
